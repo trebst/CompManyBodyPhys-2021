a10d5f10a20b411531422687bff6104c843f5793
Kai Meinerz <kmeinerz@thp.uni-koeln.de>
2021-04-08 17:43:57 +0200

    added install

---
+++ install.jl
@@ -0,0 +1,29 @@
+# Just to keep track
+
+using Pkg
+
+# Required
+Pkg.add("IJulia")
+Pkg.add("Flux")
+Pkg.add("AbstractPlotting")
+Pkg.add("GLMakie")
+Pkg.add("Colors")
+Pkg.add("CairoMakie")
+Pkg.add("DifferentialEquations")
+
+# Extras
+# Fancy extra things
+# Pkg.add("https://github.com/ffreyer/SphereSurfaceHistogram.jl.git")
+
+
+# Mentioned
+# These haven't been used but are relevant to some tasks/notes
+# Pkg.add("https://github.com/janattig/LatPhysBase.jl")
+# Pkg.add("https://github.com/janattig/LatPhysUnitcellLibrary.jl")
+# Pkg.add("https://github.com/janattig/LatPhysLatticeConstruction.jl")
+# Pkg.add("https://github.com/janattig/LatPhysLatticeModification.jl")
+# Pkg.add("https://github.com/janattig/LatPhysReciprocal.jl")
+# Pkg.add("https://github.com/janattig/LatPhysBandstructures.jl")
+# Pkg.add("https://github.com/janattig/LatPhysLuttingerTisza.jl")
+# Pkg.add("https://github.com/janattig/LatticePhysics.jl")
+# Pkg.add("StaticArrays")
